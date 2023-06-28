1d0a173081470a21dd824ace41ab75ab03d73d1f
Kirill Gusev <34633491+Maestro213@users.noreply.github.com>
2023-06-28 17:09:55 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,8 @@ import streamlit as st
 from streamlit_lightweight_charts import renderLightweightCharts
 import json
 import altair as alt
+from streamlit_lottie import st_lottie
+  
 
 import yfinance as yf
 
@@ -357,7 +359,20 @@ if Ticker in DATA["Ticker "].values:
 
 else:
    st.write("Ops, the ticker you've chosen is not available at current moment. We are working hard to improve our product and will add your desired company into our dataset")
-   
+   url = requests.get("https://iconscout.com/lottie/stock-market-chart-8833053.json")
+    # Creating a blank dictionary to store JSON file,
+    # as their structure is similar to Python Dictionary
+    url_json = dict()
+      
+    if url.status_code == 200:
+        url_json = url.json()
+    else:
+        print("Error in the URL")
+      
+  
+st.title("Adding Lottie Animation in Streamlit WebApp")
+  
+st_lottie(url_json)
    sale_val     = 10000.0
    ebitda_val   = 5000.0
    ib_val       = 2500.0
